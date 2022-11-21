1aa12101299f10b4a0b85b18dd869020ab9dcc5e
Amy Snarr <amy.snarr16@gmail.com>
2022-11-20 21:04:31 -0700

    removes classes with new hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,36 +14,42 @@ $(function () {
     })
 
     // Current hour of the day
-    var currentHour = dayjs().format('H');
+    var currentHour = dayjs().get('hour');
+    console.log(currentHour);
 
     // Adds classes depending on which hour of the day it is currently
     $(".time-block").each(function() {
         var hourNow = parseInt($(this).attr("id").replace('hour-', ''));
-        console.log (hourNow);
 
         if (hourNow < currentHour) {
             $(this).addClass("past");
-        }
-
-        else if (hourNow == currentHour) {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        } else if (hourNow == currentHour) {
             $(this).addClass("present");
-        }
-
-        else {
+            $(this).removeClass("past");
+            $(this).removeClass("future");
+        } else {
             $(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
         }
     })
 
     // Grabs previously saved items from local storage
-    $("#hour-8 .description").val(localStorage.getItem("hour-8"));
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+    $("#hour-8 .description").text(localStorage.getItem("hour-8"));
+    $("#hour-9 .description").text(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").text(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").text(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").text(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").text(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").text(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").text(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").text(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").text(localStorage.getItem("hour-17"));
+
+    if (currentHour === 1) {
+        localStorage.clear("hour-8");
+    }
 
 });
